38dcc4560902819ac60e9a0101d0ab38e0501a98
amberbrown4 <zasalarian2000@gmail.com>
2020-02-28 14:51:26 +0330

    login feature added

--- application.py
+++ application.py
@@ -39,6 +39,24 @@ def sign_up():
             db.execute("INSERT INTO users (email,password,username) VALUES (:email , :password , :username)" , {
             "email": email, "password":password , "username": username })
             db.commit()
-            return render_template("signup.html" , Username =username)
+            return render_template("signup.html" , Username =session["users"])
         else:
             return "sorry this username already exists ,you should peak another username"
+
+@app.route("/log_in", methods=["POST" , "GET"])
+def log_in():
+    if request.method == "GET":
+        return "Please sign up first!"
+    if request.method == 'POST':
+        if session.get("users") is None:
+            session["users"] = []
+        username = request.form.get("username")
+        email = request.form.get("email")
+        password = request.form.get("password")
+
+        # check if username and password are match
+        if (db.execute("SELECT password FROM users WHERE username = :username and password = :password" ,{"username":username, "password":password}).rowcount == 1):
+            session["users"].append(username)
+            return render_template("login.html" , users=session["users"])
+        else:
+            return render_template("error.html")
